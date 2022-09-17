ad85e459cbc8d10feb2e95b56d10c9082f40a87d
Ansesteri <angrylemon5678@gmail.com>
2022-09-17 16:21:57 +0300

    Add TaskH_2.py

---
+++ TaskH_2.py
@@ -0,0 +1,11 @@
+def yesterday(d, m, y):
+    """Return date of previous day
+    (day.month.year)
+
+    Args:
+        d (only from 1 to 31), m (only from 1 to 12), y (any positive number) (int)
+    
+    Returns:
+        str: date of previous day
+    """
+    pass
